df48cae24dc378043a3ffb9ad804afcac82003ae
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2024-09-30 16:13:19 +0530

    Added 404 not found page

---
+++ lms-frontend/src/Constants/CelebratyData.js
@@ -0,0 +1,39 @@
+import abdulKalam from '../assets/abdul-kalam.jpg';
+import billGates from '../assets/bill-gates.jpg';
+import nelsonMandela from '../assets/nelson-mandela.jpg';
+import einstein from '../assets/einstein.jpg';
+import steveJobs from '../assets/steve-jobs.jpg';
+
+
+export const celebraties = [
+  {
+    title: 'Nelson Mandela',
+    description: "Education is the most powerful tool you can use to change the world",
+    image : nelsonMandela,
+    slideNumber: 1
+  },
+  {
+    title: 'APJ Abdul Kalam',
+    description: "If you want to shine like a sun, first burn like a sun",
+    image : abdulKalam,
+    slideNumber: 2
+  },
+  {
+    title: 'Einstein',
+    description: "Life is like riding a bicycle. To keep your balance, you must keep moving",
+    image : einstein,
+    slideNumber: 3
+  },
+  {
+    title: 'Steve Jobs',
+    description: "You can't just ask customers what they want and then try to give that to them",
+    image : steveJobs,
+    slideNumber: 4
+  },
+  {
+    title: 'Bill Gates',
+    description: "Success is a lousy teacher. It seduces smart people into thinking they can't lose",
+    image : billGates,
+    slideNumber: 5
+  }
+]
--- lms-frontend/src/Pages/AboutUs.jsx
+++ lms-frontend/src/Pages/AboutUs.jsx
@@ -1,14 +1,44 @@
 import HomeLayout from "../Layouts/HomeLayout";
 import tree from '../assets/tree.jpg';
-import abdulKalam from '../assets/abdul-kalam.jpg';
-import billGates from '../assets/bill-gates.jpg';
-import nelsonMandela from '../assets/nelson-mandela.jpg';
-import einstein from '../assets/einstein.jpg';
-import steveJobs from '../assets/steve-jobs.jpg';
+import CarouselSlide from "../Components/CarouselSlide";
+import { celebraties } from "../Constants/CelebratyData";
 
 
 
 function AboutUs(){
+  // const celebraties = [
+  //   {
+  //     title: 'Nelson Mandela',
+  //     description: "Education is the most powerful tool you can use to change the world",
+  //     image : nelsonMandela,
+  //     slideNumber: 1
+  //   },
+  //   {
+  //     title: 'APJ Abdul Kalam',
+  //     description: "If you want to shine like a sun, first burn like a sun",
+  //     image : abdulKalam,
+  //     slideNumber: 2
+  //   },
+  //   {
+  //     title: 'Einstein',
+  //     description: "Life is like riding a bicycle. To keep your balance, you must keep moving",
+  //     image : einstein,
+  //     slideNumber: 3
+  //   },
+  //   {
+  //     title: 'Steve Jobs',
+  //     description: "You can't just ask customers what they want and then try to give that to them",
+  //     image : steveJobs,
+  //     slideNumber: 4
+  //   },
+  //   {
+  //     title: 'Bill Gates',
+  //     description: "Success is a lousy teacher. It seduces smart people into thinking they can't lose",
+  //     image : billGates,
+  //     slideNumber: 5
+  //   }
+  // ]
+
   return (
     <HomeLayout>
       <div className="pl-20 pt-20 flex flex-col text-white">
@@ -30,7 +60,9 @@ function AboutUs(){
           </div>
         </div>
         <div className="carousel w-1/2 my-16 m-auto">
-          <div id="slide1" className="carousel-item relative w-full">
+            {celebraties && celebraties.map(celebraty => (<CarouselSlide {...celebraty} key={celebraty.slideNumber} totalSlides={celebraties.length}/>))}
+          
+          {/* <div id="slide1" className="carousel-item relative w-full">
             <div className="flex flex-col items-center justify-center gap-4 px-[15%] m-auto">
               <img
                 src={nelsonMandela} className="w-40 rounded-full bordre-2 border-gray-400" />
@@ -72,34 +104,34 @@ function AboutUs(){
               </div>
             </div>
           </div>
-            <div id="slide4" className="carousel-item relative w-full">
-              <div className="flex flex-col items-center justify-center gap-4 px-[15%] m-auto">
-                <img
-                  src={steveJobs} className="w-40 rounded-full bordre-2 border-gray-400" />
-                <p className="text-xl text-gray-200">
-                "You can't just ask customers what they want and then try to give that to them"
-              </p> 
-              <h3 className="text-2xl font-semobold text-center">Steve Jobs</h3>
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                  <a href="#slide3" className="btn btn-circle">❮</a>
-                  <a href="#slide5" className="btn btn-circle">❯</a>
-                </div>
+          <div id="slide4" className="carousel-item relative w-full">
+            <div className="flex flex-col items-center justify-center gap-4 px-[15%] m-auto">
+              <img
+                src={steveJobs} className="w-40 rounded-full bordre-2 border-gray-400" />
+              <p className="text-xl text-gray-200">
+              "You can't just ask customers what they want and then try to give that to them"
+            </p> 
+            <h3 className="text-2xl font-semobold text-center">Steve Jobs</h3>
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide3" className="btn btn-circle">❮</a>
+                <a href="#slide5" className="btn btn-circle">❯</a>
               </div>
             </div>
-            <div id="slide5" className="carousel-item relative w-full">
-              <div className="flex flex-col items-center justify-center gap-4 px-[15%] m-auto">
-                <img
-                  src={billGates} className="w-40 rounded-full  bordre-2 border-gray-400" />
-                <p className="text-xl text-gray-200">
-                "Success is a lousy teacher. It seduces smart people into thinking they can't lose"
-              </p> 
-              <h3 className="text-2xl font-semobold text-center">Bill Gates</h3>
-                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                  <a href="#slide4" className="btn btn-circle">❮</a>
-                  <a href="#slide1" className="btn btn-circle">❯</a>
-                </div>
+          </div>
+          <div id="slide5" className="carousel-item relative w-full">
+            <div className="flex flex-col items-center justify-center gap-4 px-[15%] m-auto">
+              <img
+                src={billGates} className="w-40 rounded-full  bordre-2 border-gray-400" />
+              <p className="text-xl text-gray-200">
+              "Success is a lousy teacher. It seduces smart people into thinking they can't lose"
+            </p> 
+            <h3 className="text-2xl font-semobold text-center">Bill Gates</h3>
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide4" className="btn btn-circle">❮</a>
+                <a href="#slide1" className="btn btn-circle">❯</a>
               </div>
             </div>
+          </div> */}
         </div>
       </div>
     </HomeLayout>
